e7beee0e086d669b0b4672eb34706d56acb98697
Victor Ogunwehin <victorogunwehin@gmail.com>
2024-04-17 15:08:23 +0100

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { Anchor, Card, Col, Collapse, Divider, Layout, Menu, Row, Space, Table, Typography } from 'antd'
+import { Anchor, Button, Card, Col, Collapse, Divider, Form, Input, Layout, Menu, Row, Space, Steps, Table, Tag, Typography } from 'antd'
 import { Content, Header } from 'antd/es/layout/layout'
 import Sider from 'antd/es/layout/Sider'
 import { useState } from 'react'
@@ -9,26 +9,29 @@ import "./styles/dashboard.css";
 import { GiHamburgerMenu } from 'react-icons/gi'
 import { AiOutlineMoneyCollect } from 'react-icons/ai'
 import {faker} from "@faker-js/faker";
+import "./styles/dashboard.css"
+import ButtonGroup from 'antd/es/button/button-group'
 
 
 const generateData = () => {
-  const data = [];
+  const dat = [];
   for (let i = 0; i < 100; i++) {
-    data.push({
+    dat.push({
       id: faker.datatype.number(1000),
       name: faker.name.fullName(),
       email: faker.internet.email(),
       status: Math.random() > 0.5 ? true : false,
-
     })
-  }}
+  }
+  return dat;
+}
 
-
+const data = generateData();
 
 function App() {
   const [collapsed, setCollapsed] = useState(false);
 
-const {Link} = <Anchor />;
+// const { Link } = <Anchor />;
 
   return (
     <Layout className='container'>
@@ -103,22 +106,85 @@ const {Link} = <Anchor />;
               </Card>
             </Space>
             <Divider />
-
+            <Card>
+              <Form>
+                <Form.Item label={"Name"} >
+                  <Input
+                  placeholder='Name' />
+                </Form.Item>
+              </Form>
+            </Card>
+            <Card>
+              <Space>
+                <Steps 
+                  current={1} 
+                  items={[
+                    {
+                      title: "Signup",
+                      description: "Please sign up or else I'll woop your ass"
+                    },
+                    {
+                      title: "Buy subscription",
+                      description: "Buy some subscription"
+                    },
+                    {
+                      title: "Something else",
+                      description: "Do something with this"
+                    },
+              ]}/>
+              </Space>
+            </Card>
             <Row gutter={10}>
-              <Col span={6}>
+              {/* <Col span={6}>
                 <Card>
                   <Typography.Title>something</Typography.Title>
                 </Card>
-              </Col>
+              </Col> */}
               <Col span={18}>
-                <Anchor>
-                  <Link href="#test" title="Link 1" />
-                  <Link href="#test" title="Link 1" />
-                  <Link href="#test" title="Link 1" />
-                  <Link href="#test" title="Link 1" />
-                  <Link href="#test" title="Link 1" />
+                <Table 
+                  dataSource={data} 
+                  style={{ marginTop: 10, }}
+                  columns={[
+                    {
+                      dataIndex: "id",
+                      title: "ID",
+                      key: "id",
+                    },
+                    {
+                      dataIndex: "name",
+                      title: "name",
+                      key: "name",
+                    },
+                    {
+                      dataIndex: "email",
+                      title: "Email Id",
+                      key: "email",
+                    },
+                    {
+                      dataIndex: "status",
+                      title: "Email Id",
+                      render: (val) => 
+                        val ? <Tag>Active</Tag> : <Tag>Inactive</Tag>
+                    },
+                    {
+                      dataIndex: "Actions",
+                      render: () => <ButtonGroup>
+                        <Button>Edit</Button>
+                        <Button type='primary' danger>Delete</Button>
 
-                </Anchor>
+                      </ButtonGroup>
+                    
+                    },
+                  ]}
+                  />
+                 {/* <Anchor>
+                  <Link href="#components-anchor-demo-basic" title="Basic demo" />
+                   <Link href="#components-anchor-demo-static" title="Static demo" />
+                  <Link href="#API" title="API">
+                    <Link href="#Anchor-Props" title="Anchor Props" />
+                    <Link href="#Link-Props" title="Link Props" />
+                </Link>
+                </Anchor> */}
               </Col>
             </Row>
           </Content>
